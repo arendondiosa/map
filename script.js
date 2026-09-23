cf1af8f8397e25a94514e6691a806523aeee0ece
Alejandro E. Rendon <alejandro@rendon.co>
2026-09-22 20:49:20 -0500

    Update versioning for CSS and JavaScript files in index.html; modify tile layer in script.js for improved map rendering and add grayscale filter in style.css for a light basemap effect.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const COLOMBIA_CENTER = [4.5709, -74.2973];
 const map = L.map('map', { zoomControl: false }).setView(COLOMBIA_CENTER, 7);
 L.control.zoom({ position: 'bottomright' }).addTo(map);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; OpenStreetMap &copy; CARTO',
-  maxZoom: 20
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 19
 }).addTo(map);
 
 let points = [];        // {id, marker, label}
